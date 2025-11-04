8e167241ec193b349e45bda538092656c8e24934
Michael Barlow <mbarlow@rmi.org>
2025-11-03 22:17:41 -0700

    feat(stitch-core): add domain entities for resources package

---
+++ packages/stitch-core/src/stitch/core/resources/domain/entities.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+from dataclasses import dataclass
+from datetime import datetime
+from typing import TypedDict
+
+
+@dataclass(frozen=True, kw_only=True)
+class MembershipEntity:
+    id: int
+    resource_id: int
+    dataset: str
+    source_pk: str
+    created_by: str | None = None
+    status: str | None = None
+    created: datetime
+
+
+@dataclass(frozen=True, kw_only=True)
+class ResourceEntity:
+    id: int
+    repointed_id: int | None = None
+    # aggregated resources will not have
+    # a dataset an sourcepk
+    dataset: str | None = None
+    source_pk: str | None = None
+
+    # Canonical projection
+    name: str | None = None
+    country_iso3: str | None = None
+    operator: str | None = None
+    latitude: float | None = None
+    longitude: float | None = None
+    created: datetime
+
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, ResourceEntity):
+            return False
+        return self.id == other.id
+
+    def __hash__(self) -> int:
+        return hash((self.id, self.dataset, self.source_pk))
+
+
+class ResourceEntityData(TypedDict, total=False):
+    repointed_id: int | None = None
+    dataset: str | None = None
+    source_pk: str | None = None
+    name: str | None = None
+    country_iso3: str | None = None
+    operator: str | None = None
+    latitude: float | None = None
+    longitude: float | None = None
