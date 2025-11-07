38bcd43e8f88453c03d978444f90aae4d8981a28
Michael Barlow <mbarlow@rmi.org>
2025-11-07 12:33:50 -0700

    feat(resources.domain): align resource entity with current schema, add docs

--- packages/stitch-core/src/stitch/core/resources/domain/entities.py
+++ packages/stitch-core/src/stitch/core/resources/domain/entities.py
@@ -1,52 +1,68 @@
 from __future__ import annotations
+from ast import TypeAlias
 from dataclasses import dataclass
 from datetime import datetime
 from typing import TypedDict
 
-
-@dataclass(frozen=True, kw_only=True)
-class MembershipEntity:
-    id: int
-    resource_id: int
-    dataset: str
-    source_pk: str
-    created_by: str | None = None
-    status: str | None = None
-    created: datetime
+UserPlaceholder: TypeAlias = str
 
 
 @dataclass(frozen=True, kw_only=True)
 class ResourceEntity:
-    id: int
-    repointed_id: int | None = None
-    # aggregated resources will not have
-    # a dataset an sourcepk
-    dataset: str | None = None
-    source_pk: str | None = None
+    """Minimal object to relate resources to one another.
 
-    # Canonical projection
-    name: str | None = None
-    country_iso3: str | None = None
-    operator: str | None = None
-    latitude: float | None = None
-    longitude: float | None = None
+    Attributes:
+        id: unique identifier
+        repointed_id: `id` field for the new parent/aggregate resource, None if not merged
+        created: creation timestamp
+        updated: last update timestamp
+        created_by: id for the user/service/process responsible for creating the resource
+    """
+
+    resource_id: int
+    name: str
+    country: str
+    latitude: float
+    longitude: float
+    repointed_id: int | None = None
     created: datetime
+    created_by: UserPlaceholder | None = None
 
     def __eq__(self, other) -> bool:
         if not isinstance(other, ResourceEntity):
             return False
-        return self.id == other.id
+        return self.resource_id == other.resource_id
 
     def __hash__(self) -> int:
-        return hash((self.id, self.dataset, self.source_pk))
+        return hash((self.resource_id, self.dataset, self.source_pk))
 
 
-class ResourceEntityData(TypedDict, total=False):
-    repointed_id: int | None = None
-    dataset: str | None = None
-    source_pk: str | None = None
-    name: str | None = None
-    country_iso3: str | None = None
-    operator: str | None = None
-    latitude: float | None = None
-    longitude: float | None = None
+@dataclass(frozen=True, kw_only=True)
+class MembershipEntity:
+    """Primary connection between external/unknown data sources and resources.
+
+    There's a 1-to-1 mapping between "dataset" + "source_pk" and resource_id.
+
+    When merging resources together, we'll create new MembershipEntities that use the newly
+    created `resource_id`.
+
+    Splitting resources follows a similar pattern where new memberships are created that point
+    constituent source data to the appropriate resource_ids.
+
+    Attributes:
+        id:
+        resource_id:
+        dataset:
+        source_pk:
+        created_by:
+        status:
+        created:
+    """
+
+    id: int
+    resource_id: int
+    dataset: str
+    source_pk: str
+    created_by: UserPlaceholder | None = None
+    status: str | None = None
+    created: datetime
