563ae9c2681ccf89bb212578c05959c6b42b2888
Michael Barlow <mbarlow@rmi.org>
2026-01-14 10:34:10 -0700

    fix import

--- deployments/api/src/stitch/api/routers/resources.py
+++ deployments/api/src/stitch/api/routers/resources.py
@@ -2,7 +2,7 @@ from collections.abc import Sequence
 
 from fastapi import APIRouter
 
-import stitch.api.db.resources as resource_repo
+from stitch.api.db import resource_actions
 from stitch.api.db.config import UnitOfWorkDep
 from stitch.api.deps import CurrentUser
 from stitch.api.entities import CreateResource, Resource
@@ -20,14 +20,14 @@ async def get_all_resources() -> Sequence[Resource]: ...
 
 @router.get("/{id}", response_model=Resource)
 async def get_resource(*, uow: UnitOfWorkDep, id: int) -> Resource:
-    return await resource_repo.get(session=uow.session, id=id)
+    return await resource_actions.get(session=uow.session, id=id)
 
 
 @router.post("/", response_model=Resource)
 async def create_resource(
     *, uow: UnitOfWorkDep, user: CurrentUser, resource_in: CreateResource
 ) -> Resource:
-    return await resource_repo.create(
+    return await resource_actions.create(
         session=uow.session, user=user, resource=resource_in
     )
 
