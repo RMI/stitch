e8810087e639fa314f9cfe281bf25b28b174fe1e
Alex Axthelm <aaxthelm@RMI.org>
2026-02-24 14:58:50 +0100

    linting: unused import

--- deployments/api/src/stitch/api/routers/resources.py
+++ deployments/api/src/stitch/api/routers/resources.py
@@ -4,7 +4,7 @@ from collections.abc import Sequence
 
 from fastapi import APIRouter, HTTPException
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 
 from stitch.api.db import resource_actions
 from stitch.api.db.config import UnitOfWorkDep
