8d05871d3d36b8cda0ef6553076c534988994d44
jasonbrownrmi <122411427+jasonbrownrmi@users.noreply.github.com>
2026-03-09 15:47:26 -0700

    style(ui): adjust section header styles

--- deployments/stitch-frontend/src/components/SectionHeader.jsx
+++ deployments/stitch-frontend/src/components/SectionHeader.jsx
@@ -1,8 +1,8 @@
 export default function SectionHeader({ title }) {
   return (
     <div className="mb-4">
-      <h2 className="text-base font-semibold text-gray-700 text-left">{title}</h2>
-      <hr className="mt-1 border-gray-300" />
+      <h2 className="text-2xl font-semibold text-gray-dark text-left">{title}</h2>
+      <hr className="mt-1 border-gray-dark" />
     </div>
   );
 }
