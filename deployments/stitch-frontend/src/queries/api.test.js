6cafa4cd75cbae389f6d675d089fbbb36a1e2e4d
Alex Axthelm <aaxthelm@RMI.org>
2026-05-14 12:21:48 +0200

    style: format

--- deployments/stitch-frontend/src/queries/api.test.js
+++ deployments/stitch-frontend/src/queries/api.test.js
@@ -1,9 +1,5 @@
 import { describe, it, expect, vi, beforeEach } from "vitest";
-import {
-  createLLMSuggestion,
-  getResources,
-  getResource,
-} from "./api";
+import { createLLMSuggestion, getResources, getResource } from "./api";
 
 describe("API Functions", () => {
   let mockFetcher;
